e1365ed17658c3e3ff1d3e8803d23fec1f28589f
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2020-06-02 16:17:29 +0200

    Update style.js

--- style.js
+++ style.js
@@ -18,6 +18,9 @@
 				{ name: 'Título 2', element: 'h4', attributes: { 'class': 'bck-title bck-title2'} },
 				
 				{ name: 'Énfasis círculo', element: 'span', attributes: { 'class': 'bck-enfasis-1'} },
+				{ name: 'Énfasis morado', element: 'span', attributes: { 'class': 'bck-enfasis-2'} },
+				{ name: 'Énfasis naranja', element: 'span', attributes: { 'class': 'bck-enfasis-3'} },
+				{ name: 'Énfasis gris', element: 'span', attributes: { 'class': 'bck-enfasis-4'} },
 
 				{ name: 'Lista num 1', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-1' } },
 				{ name: 'Lista letras 1', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-2' } },
